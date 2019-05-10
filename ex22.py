6adf47d3413ee7b6813815ef5d1b7b98510c7f75
Connor Spillane <student.137684@worcesterschools.net>
2019-05-10 15:00:19 +0000

    ex23 - ex31

---
+++ ex22.py
@@ -0,0 +1,16 @@
+print - this prints out the string defined in quotes 
+# - comment, this allows you to ommit lines and add comments 
++ - this allows for addition 
+- - this allows for subtration
+/ - this allows for division 
+* - this allows for multiplication 
+> - returns true or false if value is greater 
+< - returns true or false if value is less than 
+% - returns the remainder of the division problem 
+= - tells if values are equal and makes variables
+<= - tells if values are less than or equal 
+>= - tells if values are greater than or equal 
+one = 1 - an example of a variable
+two = " " 
+("i have" one "dog") - example of a way to use a variable 
+(f"I can also put more than {one} variable in a string like{two}this")
--- test.py
+++
@@ -1,3 +0,0 @@
-twins = "Lil Corey and Lil Christian"
-
-print(f"They do it for the twins free {twins}")
